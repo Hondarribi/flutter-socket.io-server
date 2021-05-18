65667ee730fc429633701f9e7cf277a4e7771d07
David Sánchez Zato <dsanchez@lksnext.com>
2021-05-18 23:27:47 +0200

    Corrección Bands y Band

--- models/band.js
+++ models/band.js
@@ -1,41 +1,15 @@
-const Band = require("./band");
+const { v4: uuidV4 } = require('uuid');
 
+class Band {
 
-class Bands {
-
-    constructor() {
-        this.bands = [];
-    }
-
-    addBand( band = new Band() ) {
-        this.bands.push( band );
-    }
-
-    getBands() {
-        return this.bands;
-    }
-
-    deleteBand( id = '' ) {
-        this.bands = this.bands.filter( band => band.id !== id );
-        return this.bands;
-    }
-
-    voteBand( id = '' ) {
-
-        this.bands = this.bands.map( band => {
-
-            if ( band.id === id ) {
-                band.votes++;
-                return band;
-            } else {
-                return band;
-            }
-
-        });
-
+    constructor( name = 'no-name' ) {
+        
+        this.id = uuidV4(); // identificador único
+        this.name  = name;
+        this.votes = 0;
     }
 
 }
 
 
-module.exports = Bands;
+module.exports = Band;
--- models/bands.js
+++ models/bands.js
@@ -1,15 +1,43 @@
-const { v4: uuidV4 } = require('uuid');
 
-class Band {
 
-    constructor( name = 'no-name' ) {
-        
-        this.id = uuidV4(); // identificador único
-        this.name  = name;
-        this.votes = 0;
+const Band = require("./band");
+
+
+class Bands {
+
+    constructor() {
+        this.bands = [];
+    }
+
+    addBand( band = new Band() ) {
+        this.bands.push( band );
+    }
+
+    getBands() {
+        return this.bands;
+    }
+
+    deleteBand( id = '' ) {
+        this.bands = this.bands.filter( band => band.id !== id );
+        return this.bands;
+    }
+
+    voteBand( id = '' ) {
+
+        this.bands = this.bands.map( band => {
+
+            if ( band.id === id ) {
+                band.votes++;
+                return band;
+            } else {
+                return band;
+            }
+
+        });
+
     }
 
 }
 
 
-module.exports = Band;
+module.exports = Bands;
